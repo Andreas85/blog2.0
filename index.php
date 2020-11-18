2da4e4b72329fdaebc21ba9045633d046d5390b6
Andreas Larsson <andreas.l85@protonmail.com>
2020-11-18 16:49:49 +0000

    Adding index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script>
+      function mejl() {
+        var mejl = 'moc.liamnotorp@58l.saerdna:otliam';
+        window.location.href = mejl.split("").reverse().join("");
+      }
+    </script>
+  </head>
+  <body>
+    <div class="header container-fluid">
+      <div class="row">
+        <div class="home-link col-6">Andreas Larsson</div>
+        <div class="menu col-6">
+          <div class="row justify-content-end">
+            <div class="menu-item col-md-auto"><a href="test.php?page=blog" style="text-decoration-line: line-through">Blogg</a></div>
+            <div class="menu-item col-md-auto"><a href="https://shows.acast.com/ledarskap-och-miljon/episodes">Podcast</a></div>
+            <div class="menu-item col-md-auto"><a href="javascript: void(0)" onclick="mejl()">Kontakt</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content-wrapper container-fluid">
+      <div class="row content-text justify-content-center">
+        <div class="content col-8">
+Jag heter Andreas Larsson jag är systemutvecklare inom ehandel och podcastar om hållbarhet.
+        </div>
+      </div>
+      <div class="row content-image-and-text justify-content-center">
+        <div class="content col-5">
+          <img src="andreaslarsson_web.jpg" class="content-profile-picture">
+        </div>
+        <div class="content col-3">
+Bosatt i Stockholm. Planerar att skaffa egen lägenhet. Kanske en kattunge.
+        </div>
+      </div>
+      <div class="row content-text justify-content-center">
+        <div class="content col-8">
+Mitt största fokus nu är arbete, karriär, min ekonomi. Drömmer om passion och att lämna ett avtryck.
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+
